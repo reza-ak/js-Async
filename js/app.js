6df78f24c3241418b8a53f3421237ec4422bd1d9
reza-ak <reeza.dev@gmail.com>
2023-06-30 14:35:21 +0430

    Change location

--- js/app.js
+++ js/app.js
@@ -12,3 +12,12 @@ function getWeather() {
 }
 
 document.addEventListener('DOMContentLoaded', getWeather);
+
+document.getElementById('w-change-btn').addEventListener('click', changeLocation);
+
+function changeLocation() {
+  const city = document.getElementById('city').value;
+  const state = document.getElementById('state').value;
+  weather.changeLocation(city, state);
+  getWeather();
+}
